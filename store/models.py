3b585ba497b52bc966c9ebac258511b2fb99d3c5
tientran97 <tien.minhtran615034@gmail.com>
2023-10-10 23:02:43 +0700

    Paginator for store template

--- store/models.py
+++ store/models.py
@@ -20,6 +20,6 @@ class Product(models.Model):
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
 
-    
+
     def __str__(self):
         return self.product_name
--- store/views.py
+++ store/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from category.models import Category
 from store.models import Product
 from carts.models import CartItem
 
 from carts.views import _cart_id_by_session
+
+
 
 
 def store(request, category_slug=None):
@@ -13,16 +16,24 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by("-id")
+
+        paginator = Paginator(products, 8)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+
         product_count = products.count()
-        categories = Category.objects.all()
 
 
     context = {
-        'products': products,
-        'categories': categories,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request,'store/store.html', context)
