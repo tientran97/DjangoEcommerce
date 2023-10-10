cd9d26d667449fb6d5f60871f45a53ac1192cf08
tientran97 <tien.minhtran615034@gmail.com>
2023-10-10 22:03:27 +0700

    Complete the shopping cart functionality and added context_processors for cart_counter

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 from category.models import Category
 from store.models import Product
+from carts.models import CartItem
+
+from carts.views import _cart_id_by_session
 
 
 def store(request, category_slug=None):
@@ -28,10 +31,12 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        product_already_in_cart = CartItem.objects.filter(cart__cart_id=_cart_id_by_session(request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
-        'single_product': single_product
+        'single_product': single_product,
+        'product_already_in_cart': product_already_in_cart,
     }
     return render(request, 'store/product_detail.html', context)
