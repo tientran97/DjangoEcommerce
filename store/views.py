44783fbbae5df07403f1d2c17e0475437008ecdb
tientran97 <tien.minhtran615034@gmail.com>
2023-10-11 18:13:42 +0700

    Implement Search functionality

--- store/views.py
+++ store/views.py
@@ -6,6 +6,7 @@ from carts.models import CartItem
 
 from carts.views import _cart_id_by_session
 
+from django.db.models import Q
 
 
 
@@ -51,3 +52,15 @@ def product_detail(request, category_slug, product_slug):
         'product_already_in_cart': product_already_in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
